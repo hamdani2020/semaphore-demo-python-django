301fed7f499c54c16f9881cca26ea90191bb813b
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2024-11-15 10:52:18 +0000

    fixed dependancies

--- pydjango_ci_integration/urls.py
+++ pydjango_ci_integration/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+
 from django.contrib import admin
+from django.urls import include, path
 
 from tasks import views
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('', include('tasks.urls'))
-]
+urlpatterns = [path(r"^admin/", admin.site.urls), path("", include("tasks.urls"))]
 
 handler404 = views.Custom404.as_view()
 handler500 = views.Custom500.as_view()
